aba596110b03483e1d284c26d6e8cbe90a9a8c5e
Nathan Buchanan <nate.buchanan@me.com>
2019-10-29 19:54:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,9 @@ actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly[:railroad] = {}
-
+monopoly = {}
+  monopoly[:railroad] = {}
+  monopoly
 end
 
 def monopoly_with_second_tier
